6c93fea7b5b2634f253b1df093a33ac761d5e2f5
lijunqi <junqi.li@jimuitech.com>
2022-03-03 18:23:10 +0800

    翻转单向链表

--- src/js翻转单向链表.ts
+++ src/js翻转单向链表.ts
@@ -1,21 +1,20 @@
-/**
- * @param arr 数组 -> 链表
-*/
-
 interface ILinkListNode {
-  value: number
-  next?: ILinkListNode
+  value: number;
+  next?: ILinkListNode;
 }
 
+/**
+ * @description 数组创建链表
+ * @parms arr 数组
+*/
 function createLinkList (arr: number[]): ILinkListNode {
   const length = arr.length;
 
-  // arr = [100, 200, 300, 400]
-  // curNode = { value: 400 }
-  // 倒数第二个 = { value: 300, next: curNode }
   let curNode: ILinkListNode = {
     value: arr[length - 1]
   }
+
+  if (length === 1) return curNode;
 
   for (let i = length - 2; i >= 0; i--) {
     curNode = {
@@ -24,11 +23,41 @@ function createLinkList (arr: number[]): ILinkListNode {
     }
   }
 
+  return curNode;
+}
+
+/**
+ * @description 翻转链表
+ * @param linkListNode 链表对象
+*/
+function reverseLinkList (linkListNode: ILinkListNode): ILinkListNode {
+  // 三个变量保证nextNode不丢失
+  let preNode: ILinkListNode,
+      curNode: ILinkListNode,
+      nextNode: ILinkListNode = linkListNode;
+      
+  while(nextNode) {
+    // 删除头元素的next
+    if (curNode && !preNode) {
+      delete curNode.next;
+    }
+
+    // 翻转指针
+    if (curNode && preNode) {
+      curNode.next = preNode
+    }
+
+    preNode = curNode;
+    curNode = nextNode;
+    nextNode = nextNode.next;
+  }
+
+  curNode.next = preNode
   return curNode
 }
-const arr = [100, 200, 300, 400, 500]
-console.log(createLinkList(arr));
 
-export {}
-
-
+const linkList = createLinkList([100, 200, 300, 400, 500])
+console.log('linkList: ', linkList);
+const reverse_link_list = reverseLinkList(linkList)
+console.log('reverse_link_list: ', reverse_link_list);
+export default {}
